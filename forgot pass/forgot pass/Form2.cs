cf652ca9f44ad97cd9b26629d322438c31544ea1
Erikson Made <skullmade93@gmail.com>
2021-06-01 14:30:30 +0700

    Landing Page

---
+++ forgot pass/forgot pass/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forgot_pass
+{
+    public partial class FormLandingPage : Form
+    {
+        public FormLandingPage()
+        {
+            InitializeComponent();
+            groupBoxHeader.BackColor = Color.FromArgb(200, Color.DarkSlateBlue);
+            panelWelcome.BackColor = Color.FromArgb(200, Color.DarkSlateBlue);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
